2c2af94618f03d5773c8a0159956e95a36cf16d7
James Carbine <JamesCarbine@gmail.com>
2013-10-14 12:07:49 -0600

    parsing link header to display next and previous links

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'link_header'
+require 'cgi'
 
 class CoursesController < ApplicationController
   def index
@@ -12,14 +13,33 @@ class CoursesController < ApplicationController
 
     @courses = JSON.parse(response.body)
 
-    # link = LinkHeader.parse(response['link']).to_a
+    link = LinkHeader.parse(response['link']).to_a
 
-    # link.map do |zelda|
-    #   if zelda[1] == [["rel", "first"]]
-    # end
+    link.map do |zelda|
+      if zelda[1] == [["rel", "first"]]
+        @first_course = zelda[0]
+      end
+      if zelda[1] == [["rel", "prev"]]
+        @prev_course = zelda[0]
+      end
+      if zelda[1] == [["rel", "next"]]
+        @next_course = zelda[0]
+      end
+      if zelda[1] == [["rel", "last"]]
+        @last_course = zelda[0]
+      end
+    end
+    prev_course_info = URI.parse(@prev_course) if @prev_course
+    if prev_course_info
+      prev_course_query = prev_course_info.query
+    end
+    next_course_info = URI.parse(@next_course) if @next_course
+    if next_course_info
+      next_course_query = next_course_info.query
+    end
 
-    @prev_course_id = params[:page].present? && params[:page].to_i > 1 ? params[:page].to_i - 1 : nil
-    @next_course_id = params[:page].present? ? params[:page].to_i + 1 : 2
+    @prev_course_id = @prev_course.nil? ? nil : prev_course_query.present? ? CGI::parse(prev_course_query)['page'][0] : nil
+    @next_course_id = @next_course.nil? ? nil : next_course_query.present? ? CGI::parse(next_course_query)['page'][0] : nil
 
   end
 
