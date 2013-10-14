50a8e63a805e10ad8385d5ad078896e850a854cc
James Carbine <JamesCarbine@gmail.com>
2013-10-14 10:45:02 -0600

    passing in the page to get

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,8 +4,8 @@ class CoursesController < ApplicationController
   def index
     uri = URI('http://canvas-api.herokuapp.com/api/v1/courses')
 
-    params = { page: 1, access_token: '9be624b4d5206a178fc56921d5bf2c2a' }
-    uri.query = URI.encode_www_form(params)
+    url_params = { page: params[:page] || 1, access_token: '9be624b4d5206a178fc56921d5bf2c2a' }
+    uri.query = URI.encode_www_form(url_params)
 
     response = Net::HTTP.get_response(uri)
     @courses = JSON.parse(response.body)
