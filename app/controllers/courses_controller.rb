8f0c377ae71f0a3125c5cced0c0e28b334a7f304
James Carbine <JamesCarbine@gmail.com>
2013-10-14 11:36:11 -0600

    displaying the show course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'link_header'
 
 class CoursesController < ApplicationController
   def index
@@ -8,10 +9,29 @@ class CoursesController < ApplicationController
     uri.query = URI.encode_www_form(url_params)
 
     response = Net::HTTP.get_response(uri)
+
     @courses = JSON.parse(response.body)
+
+    # link = LinkHeader.parse(response['link']).to_a
+
+    # link.map do |zelda|
+    #   if zelda[1] == [["rel", "first"]]
+    # end
 
     @prev_course_id = params[:page].present? && params[:page].to_i > 1 ? params[:page].to_i - 1 : nil
     @next_course_id = params[:page].present? ? params[:page].to_i + 1 : 2
 
+  end
+
+  def show
+    url = 'http://canvas-api.herokuapp.com/api/v1/courses/' + params[:id]
+    uri = URI(url)
+
+    url_params = { page: params[:page] || 1, access_token: '9be624b4d5206a178fc56921d5bf2c2a' }
+    uri.query = URI.encode_www_form(url_params)
+
+    response = Net::HTTP.get_response(uri)
+
+    @course = JSON.parse(response.body)
   end
 end
